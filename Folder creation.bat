c9d5708f542dbc5a79b56acf8f2cf3955785c965
Hookabidoo <44333642+hookabidoo@users.noreply.github.com>
2022-10-11 14:04:52 +1100

    Add files via upload

---
+++ Folder creation.bat
@@ -0,0 +1,19 @@
+set /p folderName="Enter new folder name: "
+mkdir %folderName%
+cd %folderName%
+mkdir "1 - Pre-Engagement Docs"
+cd "1 - Pre-Engagement Docs
+mkdir "1. SoW"
+mkdir "2. RoE"
+cd ..
+mkdir "2 - Technical Docs"
+cd "2 - Technical Docs"
+mkdir "1 -  Client Provided Docs"
+mkdir "2 - Burp Logs"
+cd ..
+mkdir "3 - Reporting"
+cd "3 - Reporting"
+mkdir "1 - Drafts"
+mkdir "2 - Final"
+mkdir "3 - Report Key"
+cd ..
